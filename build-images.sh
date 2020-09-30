029f3f357b499edfb961a4c4df2e7b7c2d7c5a79
Dennis Hermsmeier <dennis@hermsmeier.me>
2020-09-30 06:11:34 +0200

    add php 8.0 (#19)

--- build-images.sh
+++ build-images.sh
@@ -2,11 +2,17 @@
 
 set -e
 
-LATEST="7.4"
-STABLE="7.4"
-README_URL="http://php.net/downloads.php"
+IMAGE_NAME="${IMAGE_NAME:-hermsi/alpine-fpm-php}"
 
-DIRECTORIES=($(find "${TRAVIS_BUILD_DIR}" -maxdepth 1 -mindepth 1 -type d -name "php*" -o -name "conf.d" | sed -e 's#.*\/\(\)#\1#' | sort))
+LATEST="8.0"
+STABLE="7.4"
+
+BUILD_DIR="${PWD}"
+if [[ -n "${TRAVIS_BUILD_DIR}" ]] ; then
+  BUILD_DIR="${TRAVIS_BUILD_DIR}"
+fi
+
+DIRECTORIES=($(find "${BUILD_DIR}" -maxdepth 1 -mindepth 1 -type d -name "php*" -o -name "conf.d" | sed -e 's#.*\/\(\)#\1#' | sort))
 CHANGED_DIRECTORIES=($(git -C "${TRAVIS_BUILD_DIR}" diff HEAD~ --name-only | grep -ioe "php-[0-9+].[0-9+]\|conf.d\|build-images.sh" | sort))
 
 PHPREDIS_VERSION="$(w3m -dump "https://github.com/phpredis/phpredis/releases"  | egrep "^[0-9]+.[0-9]+.[0-9]+(RC[0-9]+)?" | tr -d '\r' | awk '{print $1}' | head -n1)"
@@ -15,7 +21,6 @@ PHPREDIS_VERSION_TAG="phpredis${PHPREDIS_VERSION}"
 BUILD_ALL_REGEX=".*conf.d.*\|.*build-images.sh.*"
 
 docker_push() {
-
     unset IMAGE
     IMAGE="${1}"
 
@@ -23,8 +28,10 @@ docker_push() {
     docker push "${IMAGE}" 1>/dev/null
 }
 
-if [[ "${#CHANGED_DIRECTORIES[@]}" -eq 0 ]] || [[ $( echo "${CHANGED_DIRECTORIES[@]}" | grep -e "${BUILD_ALL_REGEX}" ) ]]; then
-    TO_BUILD=($(find "${TRAVIS_BUILD_DIR}" -maxdepth 1 -mindepth 1 -type d -name "php*" | sed -e 's#.*\/\(\)#\1#' | sort))
+if [[ -n "${1}" ]]; then
+    TO_BUILD=("${1}")
+elif [[ "${#CHANGED_DIRECTORIES[@]}" -eq 0 ]] || [[ $( echo "${CHANGED_DIRECTORIES[@]}" | grep -e "${BUILD_ALL_REGEX}" ) ]]; then
+    TO_BUILD=($(find "${BUILD_DIR}" -maxdepth 1 -mindepth 1 -type d -name "php*" | sed -e 's#.*\/\(\)#\1#' | sort))
 else
     TO_BUILD=(${CHANGED_DIRECTORIES})
 fi
@@ -35,12 +42,18 @@ echo "# ${TO_BUILD[@]}"
 
 for PHP_VERSION_DIR in ${TO_BUILD[@]}; do
 
-    unset FULL_PHP_VERSION_PATH
-    FULL_PHP_VERSION_PATH="${TRAVIS_BUILD_DIR}/${PHP_VERSION_DIR}"
+    echo "# # # # # # # # # # # # # # # # # #"
+    echo "# Building: ${PHP_VERSION_DIR}"
 
+    unset FULL_PHP_VERSION_PATH
+    FULL_PHP_VERSION_PATH="${BUILD_DIR}/${PHP_VERSION_DIR}"
+
+    unset PHP_VERSION
+    PHP_VERSION="${PHP_VERSION_DIR#*-}"
+
+    docker pull "php:${PHP_VERSION}-fpm-alpine"
     unset PATCH_RELEASE_TAG
-    docker pull php:${PHP_VERSION_DIR##*-}-fpm-alpine
-    PATCH_RELEASE_TAG="$(docker run --rm --entrypoint /usr/bin/env -t php:${PHP_VERSION_DIR##*-}-fpm-alpine /bin/sh -c 'echo $PHP_VERSION' | tr -d '\r')"
+    PATCH_RELEASE_TAG="$(docker run --rm --entrypoint /usr/bin/env -t "php:${PHP_VERSION}-fpm-alpine" /bin/sh -c 'echo $PHP_VERSION' | tr -d '\r')"
 
     unset MINOR_RELEASE_TAG
     MINOR_RELEASE_TAG="${PATCH_RELEASE_TAG%.*}"
@@ -54,12 +67,8 @@ for PHP_VERSION_DIR in ${TO_BUILD[@]}; do
     unset LATEST_RELEASE_TAG
     LATEST_RELEASE_TAG="latest"
 
-    echo "# # # # # # # # # # # # # # # # # #"
-    echo "# Building: ${PHP_VERSION_DIR}"
-
     set -x
     docker build \
-        --quiet \
         --no-cache \
         --pull \
         --build-arg PHP_VERSION="${PATCH_RELEASE_TAG}" \
@@ -75,7 +84,7 @@ for PHP_VERSION_DIR in ${TO_BUILD[@]}; do
         --tag "${IMAGE_NAME}:${PATCH_RELEASE_TAG}" \
         --tag "${IMAGE_NAME}:${PATCH_RELEASE_TAG}-${PHPREDIS_VERSION_TAG}" \
         --file "${FULL_PHP_VERSION_PATH}/Dockerfile" \
-        "${TRAVIS_BUILD_DIR}" 1>/dev/null
+        "${BUILD_DIR}"
     set +x
 
     if [[ "${TRAVIS_BRANCH}" == "master" ]] && [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
@@ -87,7 +96,6 @@ for PHP_VERSION_DIR in ${TO_BUILD[@]}; do
         docker_push "${IMAGE_NAME}:${MINOR_RELEASE_TAG}-${PHPREDIS_VERSION_TAG}"
         docker_push "${IMAGE_NAME}:${PATCH_RELEASE_TAG}"
         docker_push "${IMAGE_NAME}:${PATCH_RELEASE_TAG}-${PHPREDIS_VERSION_TAG}"
-        
 
     fi
 
